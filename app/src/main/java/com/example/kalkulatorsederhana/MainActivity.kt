9cb91e548b9a6fceb469caea3344b6230b619560
NandikaSaka <putra.2205551006@student.unud.ac.id>
2024-05-07 22:31:28 +0800

    start 3

--- app/src/main/java/com/example/kalkulatorsederhana/MainActivity.kt
+++ app/src/main/java/com/example/kalkulatorsederhana/MainActivity.kt
@@ -1,12 +1,7 @@
-@file:Suppress("NAME_SHADOWING")
-
 package com.example.kalkulatorsederhana
 
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -28,8 +23,7 @@ class MainActivity : AppCompatActivity() {
         val kali = main.times
         val bagi = main.divide
         val hasil = main.nilaiHasil
-        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
-        val btnMoveBrowser: Button = findViewById(R.id.btn_move_browser)
+
 
         tambah.setOnClickListener {
             var nilai1 = input1.text.toString()
@@ -61,7 +55,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        kali.setOnClickListener{
+        kali.setOnClickListener {
+            var nilai1 = input1.text.toString()
+            var nilai2 = input2.text.toString()
+
+            if (nilai1.isEmpty() || nilai2.isEmpty()) {
+                Toast.makeText(this, "Mohon lengkapi kedua datanya!", Toast.LENGTH_SHORT).show()
+            } else {
+                var total = nilai1.toFloat() * nilai2.toFloat()
+                hasil.text = total.toString()
+                input1.text.clear()
+                input2.text.clear()
+            }
+        }
+
+        bagi.setOnClickListener{
             var nilai1 = input1.text.toString()
             var nilai2 = input2.text.toString()
 
@@ -69,38 +77,11 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Mohon lengkapi kedua datanya!", Toast.LENGTH_SHORT).show()
             }
             else{
-                var total = nilai1.toFloat() * nilai2.toFloat()
+                var total = nilai1.toFloat() / nilai2.toFloat()
                 hasil.text = total.toString()
                 input1.text.clear()
                 input2.text.clear()
             }
-
-            bagi.setOnClickListener{
-                var nilai1 = input1.text.toString()
-                var nilai2 = input2.text.toString()
-
-                if(nilai1.isEmpty() || nilai2.isEmpty()){
-                    Toast.makeText(this, "Mohon lengkapi kedua datanya!", Toast.LENGTH_SHORT).show()
-                }
-                else{
-                    var total = nilai1.toFloat() / nilai2.toFloat()
-                    hasil.text = total.toString()
-                    input1.text.clear()
-                    input2.text.clear()
-                }
-            }
-
-            btnMoveActivity.setOnClickListener{
-                val intent = Intent(this, AnotherActivity::class.java)
-                startActivity(intent)
-            }
-
-            btnMoveBrowser.setOnClickListener{
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com"));
-                startActivity(intent)
-            }
-
-
         }
     }
 }
